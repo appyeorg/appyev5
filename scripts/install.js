460e3b2fc0217e7999b8efda8cbd9e14d1a5578b
A Duck <youarea@duck.com>
2024-06-12 09:41:39 -0400

    Add basic install process.

--- scripts/install.js
+++ scripts/install.js
@@ -1,5 +1,5 @@
 
-mirrors = [
+const mirrors = [
   "https://git.basicfan.eu.org/KUKHUA/Appye/raw/branch/main/",
   "https://raw.githubusercontent.com/appyeorg/appyev5/main/",
   "https://raw.githack.com/appyeorg/appyev5/main/",
@@ -7,7 +7,7 @@ mirrors = [
   "https://codeberg.org/lucky/Appye/raw/branch/main/",
 ]
 
-proxies = [
+const proxies = [
   "https://corsproxy.io/?",
   "https://api.allorigins.win/raw?url="
 ]
@@ -20,8 +20,12 @@ async function makeFile(opfsRoot, fileName, fileType, content) {
     // Get a writable stream
     let writable = await fileHandle.createWritable();
   
-    // Write the content to the file
-    await writable.write(new Blob([content], { type: fileType }));
+    // check if it is alreadly a blob
+    if(content instanceof Blob){
+      await writable.write(content);
+    } else {
+      await writable.write(new Blob([content], { type: fileType }));
+    }
   
     // Close the writable stream
     await writable.close();
@@ -30,14 +34,79 @@ async function makeFile(opfsRoot, fileName, fileType, content) {
     return fileHandle;
   }
 
-async function InitFs() {
+  async function InitFs() {
     const fs = await navigator.storage.getDirectory();
-    user = fs.getDirectoryHandle('user', { create: true });
-    userApps = fs.getDirectoryHandle('user/apps', { create: true });
-    userCLI = fs.getDirectoryHandle('user/cli', { create: true });
-    userSystem = fs.getDirectoryHandle('user/system', { create: true });
-    //Find a mirror.
-    for(currentMirror in mirrors){
-      
+    // We need to get a working mirror
+    let files;
+    let folders;
+    let mirror;
+    for (let currentMirror in mirrors){
+      try {
+        console.log("Trying mirror: " + mirrors[currentMirror]);
+        let filesRequest = await fetch(mirrors[currentMirror] + "files.json");
+        let foldersRequest = await fetch(mirrors[currentMirror] + "folders.json");
+        if(filesRequest.ok && foldersRequest.ok){
+          console.log("Mirror found: " + mirrors[currentMirror]);
+          mirror = mirrors[currentMirror];
+          files = await filesRequest.json();
+          folders = await foldersRequest.json();
+
+          //I don't feel like changing my shell script, this will do.
+          files = files.files;
+          folders = folders.folders;
+
+          break;
+        }
+      } catch (error) {
+        console.error("Error: " + error);
+      }
+    }
+
+    if(!files || !folders){
+      alert("No working mirrors found");
+      return;
+    }
+
+    for(let folder of folders){
+      if(folder.includes("/")){
+        console.log("Creating folder: " + folder);
+        let folderParts = folder.split("/");
+        console.log("Creating folders: ",folderParts);
+        let currentFolder = fs;
+        console.log("Current folder: ", currentFolder);
+        for(let folderPart in folderParts){
+          try {
+            if (!(await currentFolder.queryPermission({ mode: 'readwrite' }))) {
+              currentFolder = await currentFolder.getDirectoryHandle(folderPart, { create: true });
+            }
+          } catch (error) {
+            console.error("Error: " + error);
+          }
+        }
+      } else {
+        console.log("Creating folder: " + folder);
+       try {
+        if (!(await fs.queryPermission({ mode: 'readwrite' }))) {
+          await fs.getDirectoryHandle(folder, { create: true });
+        }
+       } catch (error) {
+         console.error("Error: " + error);
+      }
+    }
+
+    for(let file of files){
+      console.log("Downloading file: " + file);
+      let content = await fetch(mirror + file);
+      let contentBlob = await content.blob();
+      let contnetMimeType = content.headers.get("Content-Type");
+      console.log("File: " + file + " MimeType: " + contnetMimeType);
+      try {
+        if (!(await fs.queryPermission({ mode: 'readwrite' }))) {
+          makeFile(fs, file, contnetMimeType, contentBlob);
+        }
+      } catch (error) {
+        console.error("Error: " + error);
+      }
     }
   }
+}
